6733cd0b66eaa12af95c16727fc476e09051b302
FlorianLfts <159449152+FlorianLfts@users.noreply.github.com>
2025-03-09 17:07:24 +0100

    Update para_ti.py

--- para_ti.py
+++ para_ti.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-time import sleep
+from time import sleep
 
 # Configuration de la page
 st.set_page_config(page_title="Dashboard Streamlit", layout="wide")
